16155c5deb0e2bcb0623ba6d7d6d15343505c457
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-01-20 17:36:48 -0300

    action e página de login praticamente prontas, só faltando alguns detalhes

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,13 +1,74 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { login } from "../../actions/login";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import styled from "styled-components";
+
+const LoginForm = styled.form`
+width: 100%;
+height: 100vh;
+gap: 10px;
+place-content: center;
+justify-items: center;
+display: grid;
+`
+const Title = styled.h1 `
+width: 100%;
+height: 5vh;
+justify-content: center;
+`
 
 class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleFieldChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleLoginButton = () => {
+    const { email, password } = this.state;
+
+    this.props.login(email, password)
+  }
+
   render() {
+    const { email, password } = this.state
+
     return (
-      <div>
-        Oie
-      </div>
+      <LoginForm>
+        <Title>4eddit - Home</Title>
+        <TextField
+        onChange={this.handleFieldChange}
+        name="email"
+        type="email"
+        label="Nome de Usuário"
+        value={email} 
+        />
+
+        <TextField
+        onChange={this.handleFieldChange}
+        name="password"
+        type="password"
+        label="Senha"
+        value={password}
+        />
+        <Button onClick={this.handleLoginButton}>Login</Button>
+      </LoginForm>
     );
   }
 }
 
-export default LoginPage;
+const mapDispatchToProps = (dispatch) => ({
+  login: (email, password) => dispatch(login(email, password))
+})
+
+export default connect(null, mapDispatchToProps)(LoginPage);
